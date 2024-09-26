bafc6f60abb7d76ac0d04ffb50bf4855d5dab159
Min RK <benjaminrk@gmail.com>
2024-09-27 00:41:44 +0200

    update run_test.sh that hasn't run in some time

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -10,6 +10,9 @@ fi
 if [[ "${PKG_NAME}" == "libptscotch" ]]
 then
   python3 -c "import ctypes; ctypes.CDLL('${PREFIX}/lib/libptscotch${SHLIB_EXT}')"
+
+  mpic++ $CXXFLAGS $LDFLAGS "-I$PREFIX/include" "-L$PREFIX/lib" "${RECIPE_DIR}/test/test_ptscotch.cxx" -o test_ptscotch -DSCOTCH_PTSCOTCH -lptscotch -lptscotcherr
+  mpiexec -n 1 ./test_ptscotch
 fi
 
 
@@ -28,22 +31,10 @@ fi # scotch
 if [[ "${PKG_NAME}" == "ptscotch" ]]
 then
 
-  MPIEXEC="${RECIPE_DIR}/mpiexec.sh"
-  mpiexec() { $MPIEXEC $@; }
-
-  # These `dg*` tools do not call MPI_Finalize() in some cases and then
-  # Open MPI's mpiexec complain about that. Let them run in singleton
-  # mode, outside of mpiexec's control, with a quick workaround for
-  # docker images without `ssh`/`rsh` commands.
-  export OMPI_MCA_plm_rsh_agent=false
-
   dggath -V
   dgmap -V
   dgord -V
   dgscat -V
   dgtst -V
-
-  mpic++ $CXXFLAGS $LDFLAGS "-I$PREFIX/include" "-L$PREFIX/lib" "${RECIPE_DIR}/test/test_ptscotch.cxx" -o test_ptscotch -DSCOTCH_PTSCOTCH -lptscotch -lptscotcherr
-  mpiexec -n 1 ./test_ptscotch
 
 fi # ptscotch
